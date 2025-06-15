c1a0b43a2ec9f29901ff389eb402db4a5d23124e
muhammadabrar02 <abrarsolangi2002@gmail.com>
2025-06-15 23:55:24 +0500

    updated

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,14 +1,20 @@
+import { type FC } from 'react';
+import { type Metadata } from 'next';
+
 interface PageProps {
   params: {
     slug: string;
   };
 }
 
-export default function BlogPost({ params }: PageProps) {
+const BlogPost: FC<PageProps> = ({ params }) => {
   return (
     <div>
       <h1 className="text-3xl font-bold">Blog Post: {params.slug}</h1>
       <p>This is where the blog content will be displayed.</p>
+      <p>This is where the blog content will be displayed.</p>
     </div>
   );
-}
+};
+
+export default BlogPost;
