db7772d1c3a97b9a231f463e84e3b1c387f9bdca
muhammadabrar02 <abrarsolangi2002@gmail.com>
2025-06-26 22:24:59 +0500

    updated

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -22,6 +22,7 @@ const BlogPost: FC<PageProps> = ({ params }) => {
       <p>This is where the blog content will be displayed.</p>
       <p>This is where the blog content will be displayed.</p>
       <p>This is where the blog content will be displayed.</p>
+      <p>This is where the blog content will be displayed.</p>
     </div>
   );
 };
