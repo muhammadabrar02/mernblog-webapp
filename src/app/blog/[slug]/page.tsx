7a62ba4533decd7bd06159a11265aa25f14f69da
muhammadabrar02 <abrarsolangi2002@gmail.com>
2025-06-28 23:34:04 +0500

    updated

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -17,11 +17,6 @@ const BlogPost: FC<PageProps> = async ({ params }) => {
         practical examples, and tips to get the most out of it. Whether you&apos;re a beginner or
         an expert, there&apos;s something here for everyone.
       </p>
-      <p className="mt-4 text-lg text-gray-700">
-        In thsi article, we delve into the fascinating world of {slug}, covering key insights,
-        practical examples, and tips to get the most out of it. Whether you&apos;re a beginner or
-        an expert, there&apos;s something here for everyone.
-      </p>
     </div>
   )
 }
